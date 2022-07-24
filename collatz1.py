c991047784389c06380c87e70d033de5e0fa2d8a
Zahlenzauberer <88350652+zahlenzauberer@users.noreply.github.com>
2022-07-24 17:14:48 -0300

    Add files via upload
    As duas versões do script para gerar a sequência de Collatz.

---
+++ collatz1.py
@@ -0,0 +1,11 @@
+def collatz(x):
+    while x > 1:
+        if x % 2 == 0:
+            x = int(x/2)
+            print(x)
+        else:
+            x  = int((3*x)+1)
+            print(x)
+num = int(input("Digite um número natural: "))
+print("A sequência de Collatz para o número %d é: " %num)
+collatz(num)
